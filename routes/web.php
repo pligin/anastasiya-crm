4a6ee44e789d70c23cdbb4b70066150985def265
Mr. Pligin <i@psweb.ru>
2025-06-04 23:48:31 +0300

    Add dashboard page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('dashboard.index');
     })->name('dashboard');
 });
